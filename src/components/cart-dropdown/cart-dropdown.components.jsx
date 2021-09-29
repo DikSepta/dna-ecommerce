baa655c31194d129817da1ddc195c8d437654bb8
diksepta <dikydsn@gmail.com>
2021-09-29 21:02:02 +0700

    add selector to our cart item state and use it on Cart Icon and Cart Dropdown component

--- src/components/cart-dropdown/cart-dropdown.components.jsx
+++ src/components/cart-dropdown/cart-dropdown.components.jsx
@@ -4,6 +4,7 @@ import CartItem from "../cart-item/cart-item.components";
 import {connect} from "react-redux"
 
 import "./cart-dropdown.styles.scss"
+import { selectCartItems } from "../../redux/cart/cart.selector";
 
 const CartDropdown = ({cartItems}) => (
     <div className="cart-dropdown">
@@ -18,8 +19,8 @@ const CartDropdown = ({cartItems}) => (
     </div>
 )
 
-const mapStateToProps = ({cart:{cartItems}}) => ({
-    cartItems
+const mapStateToProps = (state) => ({
+    cartItems : selectCartItems(state)
 });
 
 export default connect(mapStateToProps)(CartDropdown);
